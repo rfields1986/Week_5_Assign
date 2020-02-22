28fc17cb5e80193674a39ce4f246ddaa31bc5b3a
Ryan Fields <rfields@my.wctc.edu>
2020-02-22 12:04:08 -0600

    1st commit

---
+++ Week_5_Assign/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week_5_Assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exit = 0, input;
+
+            
+            do
+            {
+                //ask for choice
+                Console.Clear();
+                Console.WriteLine("1. Read The File.\n2. Write a new ticket to file.\n3. Exit\n");
+                Int32.TryParse(Console.ReadLine(), out input);
+                Console.Clear();
+                switch (input)
+                {
+                    case 1:
+                        exit = 1;
+                        ReadTicketFile();
+
+                        break;
+
+                    case 2:
+                        {
+                            exit = 1;
+                            Ticket ticketEntry = new Ticket();
+                            ticketEntry.CreateNewTicket();
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            exit = 0;
+                            Console.WriteLine("Goodbye");
+                        }
+                        break;
+                }
+            } while (exit != 0);
+        }
+        public static void ReadTicketFile()
+        {
+            string file = "Tickets.csv";
+            StreamReader rd = new StreamReader(file);
+            string line = rd.ReadLine();
+            string[] header = line.Split(',');
+            while (!rd.EndOfStream)
+            {
+                string line1 = rd.ReadLine();
+                string[] body = line1.Split(',');
+                for (int i = 0; i < body.Length; i++)
+                {
+                    Console.Write("{0,-20}", header[i]);
+                    Console.WriteLine(body[i]);
+
+                }
+                Console.WriteLine("\n");
+
+
+
+
+            }
+            Console.WriteLine("Press Enter To Return To The Main Menu");
+            Console.ReadKey();
+            rd.Close();
+        }
+    }
+}
+
---
+++ Week_5_Assign/Ticket.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Week_5_Assign
+{
+    class Ticket
+    {
+        public string ticketID { get; set; }
+        public string ticketSummary { get; set; }
+        public string ticketStatus { get; set; }
+        public string ticketPriority { get; set; }
+        public string submitedBy { get; set; }
+        public string assignedTo { get; set; }
+        public string watchedBy { get; set; }
+
+        public Ticket(string ticketID, string ticketSummary, string ticketStatus, string ticketPriority, string submittedBy, string assignedTo, string watchedBy)
+        {
+            this.ticketID = ticketID;
+            this.ticketSummary = ticketSummary;
+            this.ticketStatus = ticketStatus;
+            this.ticketPriority = ticketPriority;
+            this.submitedBy = submittedBy;
+            this.assignedTo = assignedTo;
+            this.watchedBy = watchedBy;
+
+
+        }
+        public Ticket()
+        {
+            ticketID = "";
+            ticketSummary = "";
+            ticketStatus = "";
+            ticketPriority = "";
+            submitedBy = "";
+            assignedTo = "";
+            watchedBy = "";
+
+        }
+        public void CreateNewTicket()
+        {
+            string file = "Tickets.csv";
+            StreamWriter rd1 = new StreamWriter(file, append: true);
+            Console.WriteLine("Please enter your ticket number?");
+            ticketID = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please enter a summary of the issue?");
+            ticketSummary = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("What the status of this ticket?");
+            ticketStatus = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please enter a priority: High, Medium, or Low?");
+            ticketPriority = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("What is your name?");
+            submitedBy = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Who is the ticket assigned too?");
+            assignedTo = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Who will be watching this ticket? Seperate multiple watchers with \"|\"");
+            watchedBy = Console.ReadLine();
+            Console.Clear();
+            rd1.WriteLine($"{ticketID},{ticketSummary},{ticketStatus},{ticketPriority},{submitedBy},{assignedTo},{watchedBy}");
+            rd1.Close();
+            Console.WriteLine("Press Enter To Return To The Main Menu");
+            Console.ReadKey();
+
+        }
+    }
+}
